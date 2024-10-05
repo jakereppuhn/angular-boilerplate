5ae06b084589f4ad7ba0b36859774de1b9c6868d
jacobreppuhn <jakereppuhn@gmail.com>
2024-10-05 07:39:30 -0400

    configured eslint, prettier & tailwind

---
+++ eslint.config.js
@@ -0,0 +1,70 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+const nPlugin = require("eslint-plugin-n");
+const checkFilePlugin = require("eslint-plugin-check-file");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: {
+      n: nPlugin,
+      "check-file": checkFilePlugin,
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      "prefer-arrow-callback": ["error"],
+      "prefer-template": ["error"],
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "n/no-process-env": ["error"],
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
+        },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,html}": "KEBAB_CASE",
+        },
+        {
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
