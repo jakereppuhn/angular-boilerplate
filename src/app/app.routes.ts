c333c003da7a7c80dd132599183997f4b9886e5c
jacobreppuhn <jakereppuhn@gmail.com>
2024-10-05 09:57:57 -0400

    configured basic routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./features/dashboard/dashboard.component";
+import { SettingsComponent } from "./features/settings/settings.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: DashboardComponent
+  },
+  {
+    path: "settings",
+    component: SettingsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
